e156ff6eceb70a327704482a58260ec2fcd608cc
Wu-LQUE <3371218044@qq.com>
2025-10-20 22:23:06 +0800

    递归实现树同构判断(有大小限制)

--- BinTree_Array.c
+++ BinTree_Array.c
@@ -13,38 +13,61 @@ struct TreeNode
 
 Tree BuildTree(struct TreeNode T[]){
     int nodeNum;
-    scanf("%d",&nodeNum);
+    //不要少了\n!!!!!这个不读会有奇奇怪怪的报错
+    scanf("%d\n",&nodeNum);
+    int root = Null;
     if(nodeNum){
-        int check[] = (int*) malloc(sizeof(int)*nodeNum);
+        // int *check = (int*) malloc(sizeof(int)*nodeNum);
+        int check[8];
         for(int i = 0;i<nodeNum;i++) check[i] = 0;
         for(int i = 0;i<nodeNum;i++){
-            Tree cl,cr;
-            scanf("%c %c %c",&T[i].element,&cl,&cr);
+            char cl,cr;
+            scanf("%c %c %c\n",&T[i].element,&cl,&cr);
             if(cl!='-'){
+                cl = cl - '0';
                 T[i].Left=cl;
                 check[cl]=1;
             }else{
                 T[i].Left=Null;
             }
             if(cr!='-'){
-                T[i].Right=cl;
+                cr = cr - '0';
+                T[i].Right=cr;
                 check[cr]=1;
             }else{
                 T[i].Right=Null;
             }
         }
-        int root = -1;
         for(int i = 0;i<nodeNum;i++){
             if(!check[i]){
                 root = i;
             }
         }
-        return root;
     }
+    return root;
+}
+int IsOmophic(Tree R1, Tree R2){
+    if(R1 == Null && R2 == Null) 
+        return 1;
+    if((R1 == Null && R2 != Null) || (R1 != Null && R2 == Null)) 
+        return 0;
+    if(T1[R1].element != T2[R2].element) 
+        return 0;
+    
+    // 不交换左右子树的情况
+    if(IsOmophic(T1[R1].Left, T2[R2].Left) && IsOmophic(T1[R1].Right, T2[R2].Right))
+        return 1;
+    
+    // 交换左右子树的情况
+    if(IsOmophic(T1[R1].Left, T2[R2].Right) && IsOmophic(T1[R1].Right, T2[R2].Left))
+        return 1;
+    
+    return 0;
 }
 int main(){
     Tree R1,R2;
+    freopen("BinTree_Array_test.txt","r",stdin);
     R1 = BuildTree(T1);
     R2 = BuildTree(T2);
-    
+    printf("%d",IsOmophic(R1,R2));
 }
